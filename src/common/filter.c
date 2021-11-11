aeaeed21198d6f41d0cf70bda63fe0f424922ac5
Tim Wojtulewicz <timwoj@gmail.com>
2021-11-11 19:04:28 +0000

    ZEEK-ONLY: Disable initialization of evfilt_proc
    Libkqueue on Linux with kernel versions < 5.3 uses SIGCHLD to detect whether
    child processes die. It uses this in a single thread for the evfilt_proc
    filter for kqueue(). Unfortunately their use of this conflicts with Zeek’s
    supervisor code, which also uses SIGCHLD to detect when nodes in the
    supervisor tree die. Due to the initialization order required for everything
    to come up correctly, Libkqueue overrides supervisor’s setup for that signal,
    and so supervisor doesn’t properly detect nodes exiting and they don’t get
    revived correctly.
    
    This commit simply comments out the initialization of that filter, since
    we’re not using it and nothing breaks without it existing. The filter was
    added as part of Libkqueue v2.5, and our upgrade to that version broke some
    of Zeek’s btests.

--- src/common/filter.c
+++ src/common/filter.c
@@ -97,7 +97,7 @@ filter_register_all(struct kqueue *kq)
     rv += filter_register(kq, &evfilt_write);
     rv += filter_register(kq, &evfilt_signal);
     rv += filter_register(kq, &evfilt_vnode);
-    rv += filter_register(kq, &evfilt_proc);
+//    rv += filter_register(kq, &evfilt_proc);
     rv += filter_register(kq, &evfilt_timer);
     rv += filter_register(kq, &evfilt_user);
     if (rv != 0) {
